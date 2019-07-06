a8060e2e90e71d3367af6cc5a39810e200876a31
Joseph Morse <tamed.lionheart@gmail.com>
2019-07-06 15:19:16 -0400

    test(RuleFinder): added test for getUndefinedRules

--- src/rule-finder.test.ts
+++ src/rule-finder.test.ts
@@ -1,27 +1,33 @@
 import { RuleFinder } from './rule-finder';
 import { resolve } from 'path';
 
+const withPlugins = resolve(__dirname, './test-fixutes/withPlugin.js');
+const withoutPlugins = resolve(__dirname, './test-fixutes/withoutPlugins.js');
+
 describe('RuleFinder', () => {
   test('RuleFinder without plugins, should have all the base rules', () => {
-    const ruleFinder = new RuleFinder(
-      resolve(__dirname, './test-fixutes/withoutPlugins.js')
-    );
+    const ruleFinder = new RuleFinder(withoutPlugins);
 
     expect(ruleFinder.getRules().size).toBeGreaterThanOrEqual(200);
   });
 
   test('RuleFinder with a config including plugins, has more rules', () => {
-    const withPlugins = new RuleFinder(
-      resolve(__dirname, './test-fixutes/withPlugin.js')
-    );
+    const ruleFinderWithPlugins = new RuleFinder(withPlugins);
 
-    const withoutPlugins = new RuleFinder(
-      resolve(__dirname, './test-fixutes/withoutPlugins.js')
-    );
+    const ruleFinderWithoutPlugins = new RuleFinder(withoutPlugins);
 
-    expect(withPlugins.getRules().size).toBeGreaterThan(
-      withoutPlugins.getRules().size
+    expect(ruleFinderWithPlugins.getRules().size).toBeGreaterThan(
+      ruleFinderWithoutPlugins.getRules().size
     );
+  });
+
+  test('#getRules with includeDeprecated set to false should be less then then when true', () => {
+    const ruleFinder = new RuleFinder(withoutPlugins);
+
+    const includeDeprecated = ruleFinder.getRules(true);
+    const noDeprecated = ruleFinder.getRules(false);
+
+    expect(noDeprecated.size).toBeLessThan(includeDeprecated.size);
   });
 
   test('#currentRules should only contained the defined rules', () => {
@@ -43,5 +49,31 @@ describe('RuleFinder', () => {
 
     expect(deprecated.size).toBe(1);
     expect(deprecated.has('valid-jsdoc')).toBeTruthy();
+  });
+});
+
+describe('#getUndefinedRules', () => {
+  let config: RuleFinder;
+
+  beforeAll(() => {
+    config = new RuleFinder(withoutPlugins);
+  });
+
+  test('finds unused rules', () => {
+    const unusedRules = config.getUndefinedRules(false);
+    const allRules = config.getRules(false);
+
+    expect(unusedRules.size).toBeLessThan(allRules.size);
+  });
+
+  test('finds unused rules that are deprecated', () => {
+    const unusedRules = config.getUndefinedRules(true);
+    const unusedRulesExcludeDeprecated = config.getUndefinedRules(false);
+    const allRules = config.getRules(true);
+
+    expect(unusedRules.size).toBeGreaterThanOrEqual(
+      unusedRulesExcludeDeprecated.size
+    );
+    expect(unusedRules.size).toBeLessThan(allRules.size);
   });
 });
